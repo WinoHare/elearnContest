2c7dd36aa54d2b398979a4502aa71899f7b4a7a5
Kirionenko <misha.kirionenko03@gmail.com>
2022-11-11 13:47:50 +0500

    2.1.3

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import csv
 import math
 from openpyxl import Workbook
 from openpyxl.styles import Font, Border, Side
-import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
+import pdfkit
+from jinja2 import Environment, FileSystemLoader
 
 
 class DataSet:
@@ -88,9 +88,8 @@ class InputConnect:
 
 
 class Statistics:
-    def __init__(self):
-        self.input_values = InputConnect()
-        self.vacancies = DataSet(self.input_values.file_name).vacancies_objects
+    def __init__(self, input_values):
+        self.vacancies = DataSet(input_values.file_name).vacancies_objects
         self.salary_by_year = {}
         self.count_by_year = {}
         self.prof_salary_by_year = {}
@@ -98,13 +97,13 @@ class Statistics:
         self.salary_by_city = {}
         self.count_by_city = {}
 
-    def get_statistics(self) -> None:
+    def get_statistics(self, vacancie_to_stats) -> None:
         for vacancy in self.vacancies:
             self.update_stats(vacancy.published_at, vacancy.salary.average_salary, self.salary_by_year)
             self.update_stats(vacancy.published_at, 1, self.count_by_year)
             self.update_stats(vacancy.area_name, vacancy.salary.average_salary, self.salary_by_city)
             self.update_stats(vacancy.area_name, 1, self.count_by_city)
-            if self.input_values.vacancy_name in vacancy.name:
+            if vacancie_to_stats in vacancy.name:
                 self.update_stats(vacancy.published_at, vacancy.salary.average_salary, self.prof_salary_by_year)
                 self.update_stats(vacancy.published_at, 1, self.prof_count_by_year)
 
@@ -112,7 +111,6 @@ class Statistics:
         self.get_average_salary(self.prof_salary_by_year, self.prof_count_by_year)
         self.get_average_salary(self.salary_by_city, self.count_by_city)
         self.get_percentage_of_total(self.count_by_city, len(self.vacancies))
-
 
         self.get_cities_with_enough_count()
         self.sort_statistics()
@@ -164,77 +162,78 @@ class Statistics:
         print('Доля вакансий по городам (в порядке убывания):', self.count_by_city)
 
 
-class Report:
-    def __init__(self, statistics: Statistics):
-        self.statistics = statistics
-        self.statistics.get_statistics()
+class GraphsCreator:
+    def __init__(self, stats, vacancie_name):
+        self.generate_image(stats, vacancie_name)
 
-        self.initialize_workbook()
-        self.generate_image()
-
-    def initialize_workbook(self):
-        workbook, years_sheet, cities_sheet = self.create_workbook()
-        self.add_stats_to_excel(years_sheet, cities_sheet)
-        self.set_sheets_settings(years_sheet, cities_sheet)
-
-        workbook.save('report.xlsx')
-
-    def generate_image(self):
+    def generate_image(self, stats, vacancie_name):
         plt.subplots(figsize=(10, 7))
         plt.grid(True)
 
-        self.create_salary_by_year_plot()
-        self.create_count_by_year_plot()
-        self.create_salary_by_city_plot()
-        self.create_count_by_city_plot()
+        self.create_salary_by_year_plot(stats, vacancie_name)
+        self.create_count_by_year_plot(stats, vacancie_name)
+        self.create_salary_by_city_plot(stats)
+        self.create_count_by_city_plot(stats)
 
         plt.subplots_adjust(wspace=0.5, hspace=0.5)
         plt.savefig('graph.png')
 
-    def create_salary_by_year_plot(self):
+    def create_salary_by_year_plot(self, stats, vacancie_name):
         first = plt.subplot(221)
         plt.tick_params(axis='x', which='major', labelsize=8, rotation=90)
         plt.tick_params(axis='y', which='major', labelsize=8)
-        first.bar(list(map(lambda y: y - 0.2, self.statistics.salary_by_year.keys())),
-                  self.statistics.salary_by_year.values(), width=0.4,
+        first.bar(list(map(lambda y: y - 0.2, stats.salary_by_year.keys())),
+                  stats.salary_by_year.values(), width=0.4,
                   label='Средняя з/п')
-        first.bar(list(map(lambda y: y + 0.2, self.statistics.prof_salary_by_year.keys())),
-                  self.statistics.prof_salary_by_year.values(), width=0.4,
-                  label=f'З/п {self.statistics.input_values.vacancy_name}')
+        first.bar(list(map(lambda y: y + 0.2, stats.prof_salary_by_year.keys())),
+                  stats.prof_salary_by_year.values(), width=0.4,
+                  label=f'З/п {vacancie_name}')
         plt.legend(fontsize=8)
         plt.title('Уровень зарплат по годам', fontsize=12)
 
-    def create_count_by_year_plot(self):
+    def create_count_by_year_plot(self, stats, vacancie_name):
         second = plt.subplot(222)
         plt.tick_params(axis='x', which='major', labelsize=8, rotation=90)
         plt.tick_params(axis='y', which='major', labelsize=8)
-        second.bar(list(map(lambda y: y - 0.2, self.statistics.count_by_year.keys())),
-                   self.statistics.count_by_year.values(), width=0.4,
+        second.bar(list(map(lambda y: y - 0.2, stats.count_by_year.keys())),
+                   stats.count_by_year.values(), width=0.4,
                    label='Количество вакансий')
-        second.bar(list(map(lambda y: y + 0.2, self.statistics.prof_count_by_year.keys())),
-                   self.statistics.prof_count_by_year.values(), width=0.4,
-                   label=f'Количество вакансий {self.statistics.input_values.vacancy_name}')
+        second.bar(list(map(lambda y: y + 0.2, stats.prof_count_by_year.keys())),
+                   stats.prof_count_by_year.values(), width=0.4,
+                   label=f'Количество вакансий {vacancie_name}')
         plt.legend(fontsize=8)
         plt.title('Количество вакансий по годам', fontsize=12)
 
-    def create_salary_by_city_plot(self):
+    def create_salary_by_city_plot(self, stats):
         third = plt.subplot(223)
         plt.tick_params(axis='x', which='major', labelsize=8)
         plt.tick_params(axis='y', which='major', labelsize=6)
-        third.barh(list(reversed(self.statistics.salary_by_city.keys())),
-                   list(reversed(self.statistics.salary_by_city.values())))
+        third.barh(list(reversed(stats.salary_by_city.keys())),
+                   list(reversed(stats.salary_by_city.values())))
         plt.title('Уровень зарплат по городам', fontsize=12)
 
-    def create_count_by_city_plot(self):
-        self.statistics.get_percent_of_other_cities()
+    def create_count_by_city_plot(self, stats):
+        stats.get_percent_of_other_cities()
         fourth = plt.subplot(224)
         plt.rc('xtick', labelsize=6)
-        fourth.pie(list(map(lambda c: round(c * 100, 2), self.statistics.count_by_city.values())),
-                   labels=self.statistics.count_by_city.keys(),
+        fourth.pie(list(map(lambda c: round(c * 100, 2), stats.count_by_city.values())),
+                   labels=stats.count_by_city.keys(),
                    colors=['r', 'g', 'b', 'm', 'y', 'c', 'orange', 'darkblue', 'pink', 'sienna', 'grey'])
         plt.title('Доля вакансий по городам', fontsize=12)
 
-    def create_workbook(self) -> tuple:
+
+class ExcelCreator:
+    def __init__(self, stats: Statistics, vacancie_name):
+        self.workbook = self.initialize_workbook(stats, vacancie_name)
+
+    def initialize_workbook(self, stats: Statistics, vacancie_name):
+        workbook, years_sheet, cities_sheet = self.create_workbook(vacancie_name)
+        self.add_stats_to_excel(stats, years_sheet, cities_sheet)
+        self.set_sheets_settings(stats, years_sheet, cities_sheet)
+        workbook.save('report.xlsx')
+        return workbook
+
+    def create_workbook(self, vacancie_name) -> tuple:
         workbook = Workbook()
 
         years_sheet = workbook.active
@@ -242,28 +241,28 @@ class Report:
         cities_sheet = workbook.create_sheet('Статистика по городам')
 
         years_sheet.append(
-            ['Год', 'Средняя зарплата', f'Средняя зарплата - {self.statistics.input_values.vacancy_name}',
-             'Количество вакансий', f'Количество вакансий - {self.statistics.input_values.vacancy_name}'])
+            ['Год', 'Средняя зарплата', f'Средняя зарплата - {vacancie_name}',
+             'Количество вакансий', f'Количество вакансий - {vacancie_name}'])
         cities_sheet.append(['Город', 'Уровень зарплат', '', 'Город', 'Доля Вакансий'])
 
         return workbook, years_sheet, cities_sheet
 
-    def add_stats_to_excel(self, years_sheet, cities_sheet):
-        for year in self.statistics.salary_by_year.keys():
+    def add_stats_to_excel(self, stats: Statistics, years_sheet, cities_sheet):
+        for year in stats.salary_by_year.keys():
             years_sheet.append([year,
-                                     self.statistics.salary_by_year[year],
-                                     self.statistics.prof_salary_by_year[year],
-                                     self.statistics.count_by_year[year],
-                                     self.statistics.prof_salary_by_year[year]])
+                                stats.salary_by_year[year],
+                                stats.prof_salary_by_year[year],
+                                stats.count_by_year[year],
+                                stats.prof_salary_by_year[year]])
 
-        for city in self.statistics.salary_by_city.keys():
-            cities_sheet.append([city, self.statistics.salary_by_city[city]])
+        for city in stats.salary_by_city.keys():
+            cities_sheet.append([city, stats.salary_by_city[city]])
 
-        for i, city in enumerate(self.statistics.count_by_city.keys(), 2):
+        for i, city in enumerate(stats.count_by_city.keys(), 2):
             cities_sheet[f'D{i}'].value = city
-            cities_sheet[f'E{i}'].value = f'{round(self.statistics.count_by_city[city] * 100, 2)}%'
+            cities_sheet[f'E{i}'].value = f'{round(stats.count_by_city[city] * 100, 2)}%'
 
-    def set_sheets_settings(self, years_sheet, cities_sheet) -> None:
+    def set_sheets_settings(self, stats: Statistics, years_sheet, cities_sheet) -> None:
         used_columns = ['A', 'B', 'C', 'D', 'E']
         for i in used_columns:
             years_sheet[f'{i}1'].font = Font(bold=True)
@@ -274,14 +273,64 @@ class Report:
 
         thins = Side(border_style="thin")
         for column in used_columns:
-            for row in range(1, len(self.statistics.salary_by_year.keys()) + 2):
+            for row in range(1, len(stats.salary_by_year.keys()) + 2):
                 years_sheet[f'{column}{row}'].border = Border(top=thins, bottom=thins, left=thins, right=thins)
 
         for column in used_columns:
-            for row in range(1, len(self.statistics.salary_by_city.keys()) + 2):
+            for row in range(1, len(stats.salary_by_city.keys()) + 2):
                 if column == 'C':
                     break
                 cities_sheet[f'{column}{row}'].border = Border(top=thins, bottom=thins, left=thins, right=thins)
 
-statistics = Statistics()
-report = Report(statistics)
+
+class PdfCreator:
+    def __init__(self, vacancie_name, workbook, years_sheet_rows, cities_sheet_rows):
+        config = pdfkit.configuration(wkhtmltopdf=r'D:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+        options = {'enable-local-file-access': None}
+        pdfkit.from_string(self.fill_pdf_template(vacancie_name,
+                                                  workbook["Статистика по годам"],
+                                                  workbook["Статистика по городам"],
+                                                  years_sheet_rows,
+                                                  cities_sheet_rows),
+                           'report.pdf', configuration=config, options=options)
+
+    def fill_pdf_template(self, vacancie_name, years_sheet, cities_sheet, years_sheet_rows, cities_sheet_rows):
+        env = Environment(loader=FileSystemLoader('.'))
+        template = env.get_template('pdf_template.html')
+        pdf_template = template.render({'vacancie_name': vacancie_name,
+                                        'years_table': self.create_html_table(years_sheet, years_sheet_rows),
+                                        'cities_table_first': self.create_html_table(cities_sheet,
+                                                                                     cities_sheet_rows, last_column=2),
+                                        'cities_table_second': self.create_html_table(cities_sheet,
+                                                                                      cities_sheet_rows, 4)})
+        return pdf_template
+
+    def create_html_table(self, ws, rows_count, first_column = 1, last_column=5):
+        html = ''
+        is_first = True
+        for row in ws.iter_rows(min_row=1, min_col=first_column, max_col=last_column, max_row=rows_count + 1):
+            html += '<tr>'
+            for cell in row:
+                if cell.value is None or cell.value == '':
+                    html += "<td>" + '   ' + "</td>"
+                else:
+                    html += '<td><b>' + str(cell.value) + '</b></td>' if is_first else '<td>' + str(
+                        cell.value) + '</td>'
+            html += '</tr>'
+            is_first = False
+        return html
+
+
+class Report:
+    def __init__(self):
+        self.input_values = InputConnect()
+        self.statistics = Statistics(self.input_values)
+        self.statistics.get_statistics(self.input_values.vacancy_name)
+
+        workbook = ExcelCreator(self.statistics, self.input_values.vacancy_name)
+        graph_image = GraphsCreator(self.statistics, self.input_values.vacancy_name)
+        pdf = PdfCreator(self.input_values.vacancy_name, workbook.workbook, len(self.statistics.salary_by_year.keys()),
+                         len(self.statistics.salary_by_city.keys()))
+
+
+report = Report()
